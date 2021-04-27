66bcdef2cd18ff5d08b06be12c7263de496307d5
Bruno Oliveira <321.bruno@gmail.com>
2021-04-27 10:47:00 -0300

    add info payment in admin order

---
+++ app/code/community/Integrai/Core/Block/Adminhtml/Sales/Order/View/Info/Payment.php
@@ -0,0 +1,24 @@
+<?php
+class Integrai_Core_Block_Adminhtml_Sales_Order_View_Info_Payment extends Mage_Core_Block_Template {
+
+    protected function _getHelper()
+    {
+        return Mage::helper('integrai');
+    }
+
+    public function getOrder() {
+        if (is_null($this->order)) {
+            if (Mage::registry('current_order')) {
+                $order = Mage::registry('current_order');
+            }
+            elseif (Mage::registry('order')) {
+                $order = Mage::registry('order');
+            }
+            else {
+                $order = new Varien_Object();
+            }
+            $this->order = $order;
+        }
+        return $this->order;
+    }
+}
--- app/code/community/Integrai/Core/Model/Observer.php
+++ app/code/community/Integrai/Core/Model/Observer.php
@@ -248,7 +248,7 @@ class Integrai_Core_Model_Observer
 
     public function getSalesOrderViewInfo(Varien_Event_Observer $observer) {
         $block = $observer->getBlock();
-        if (($block->getNameInLayout() == 'order_info') && ($child = $block->getChild('integrai.order.info.custom.block'))) {
+        if (($block->getNameInLayout() == 'order_info') && ($child = $block->getChild('integrai.order.info.custom.block')) || ($child = $block->getChild('integrai.order.info.custom.payment'))) {
             $transport = $observer->getTransport();
             if ($transport) {
                 $html = $transport->getHtml();
