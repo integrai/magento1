6ddc68011735e07ab922610e007e8aa716298374
Bruno Oliveira <321.bruno@gmail.com>
2021-07-09 13:19:50 -0300

    fix controllers

--- app/code/community/Integrai/Core/controllers/BoletoController.php
+++ app/code/community/Integrai/Core/controllers/BoletoController.php
@@ -10,36 +10,27 @@ class Integrai_Core_BoletoController
 
     public function indexAction()
     {
-        // check origin
-//        if ($_SERVER['SERVER_NAME'] === $this->_getHelper()->getGlobalConfig('apiUrl')) {
-
         try{
-            $order_id = trim($_GET['order_id']);
-            $is_duplicate = (bool)$_GET['is_duplicate'];
+            $order_id = $this->getRequest()->getParam('order_id');
+            $is_duplicate = (bool)$this->getRequest()->getParam('is_duplicate');
 
             $this->_getHelper()->log('Buscando boleto url do pedido: ', $order_id);
 
             $api = Mage::getModel('integrai/api');
-            $response_boleto = $api->request('/store/boleto', 'GET', null, array(
+            $response = $api->request('/store/boleto', 'GET', null, array(
                 'orderId' => $order_id,
                 'isDuplicate' => $is_duplicate
             ));
 
             $this->getResponse()->setHeader('Content-type', 'application/json');
-            $this->getResponse()->setBody(Mage::helper('core')->jsonEncode(array(
-                'boleto_url' => $response_boleto['boletoUrl']
-            )));
+            $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($response));
         } catch (Exception $e) {
             $this->_getHelper()->log('Error ao buscar boleto', $e->getMessage());
+            $this->getResponse()->setHeader('Content-type', 'application/json');
             $this->getResponse()->setHttpResponseCode(400)->setBody(Mage::helper('core')->jsonEncode(array(
-                "ok" => false,
+                "boletoUrl" => null,
                 "error" => $e->getMessage()
             )));
-            $this->_redirect("/");
         }
-
-//        } else {
-//            $this->_redirect("/");
-//        }
     }
 }
--- app/code/community/Integrai/Core/controllers/PixController.php
+++ app/code/community/Integrai/Core/controllers/PixController.php
@@ -3,36 +3,33 @@
 class Integrai_Core_PixController
     extends Mage_Core_Controller_Front_Action
 {
-//    protected function _getHelper()
-//    {
-//        return Mage::helper('integrai');
-//    }
+    protected function _getHelper()
+    {
+        return Mage::helper('integrai');
+    }
 
     public function indexAction()
     {
-        die('FOI');
-//        try{
-//            $order_id = trim($_GET['order_id']);
-////            $order_id = $this->getRequest()->getParams();
-//
-//            $this->_getHelper()->log('Buscando pix url do pedido: ', $order_id);
-//
-////            $api = Mage::getModel('integrai/api');
-////            $response = $api->request('/store/pix', 'GET', null, array(
-////                'orderId' => $order_id,
-////            ));
-//
-//            $this->getResponse()->setHeader('Content-type', 'application/json');
-//            $this->getResponse()->setBody(Mage::helper('core')->jsonEncode(array(
-//                "order_id" => $order_id
-//            )));
-//        } catch (Exception $e) {
-//            $this->_getHelper()->log('Error ao buscar pix', $e->getMessage());
-//            $this->getResponse()->setHttpResponseCode(400)->setBody(Mage::helper('core')->jsonEncode(array(
-//                "ok" => false,
-//                "error" => $e->getMessage()
-//            )));
-////            $this->_redirect("/");
-//        }
+        try{
+            $order_id = $this->getRequest()->getParam('order_id');
+
+            $this->_getHelper()->log('Buscando pix url do pedido: ', $order_id);
+
+            $api = Mage::getModel('integrai/api');
+            $response = $api->request('/store/pix', 'GET', null, array(
+                'orderId' => $order_id,
+            ));
+
+            $this->getResponse()->setHeader('Content-type', 'application/json');
+            $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($response));
+        } catch (Exception $e) {
+            $this->_getHelper()->log('Error ao buscar pix', $e->getMessage());
+            $this->getResponse()->setHeader('Content-type', 'application/json');
+            $this->getResponse()->setHttpResponseCode(400)->setBody(Mage::helper('core')->jsonEncode(array(
+                "qrCode" => null,
+                "qrCodeBase64" => null,
+                "error" => $e->getMessage()
+            )));
+        }
     }
 }
